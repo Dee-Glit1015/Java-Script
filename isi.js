5bd3c28d3e6d8b28da243ed616dc7d54b6e4793c
Desy Permata Sari <119993025+Dee-Glit1015@users.noreply.github.com>
2025-05-29 18:36:44 +0800

    Java Script yang diletakan di File lain

---
+++ isi.js
@@ -0,0 +1,11 @@
+document.writeln("<CENTER>");
+            document.writeln("<HR WIDTH = 600 COLOR=GREEN>")
+            document.writeln("<H1>INSTITUT TEKNOLOGI NASIONAL MALANG <H1>")
+            document.writeln("<H2>FAKULTAS TEKNIK INDUSTRI<H2>");           
+            document.writeln("Selamat Datang Di Java Script");
+            document.writeln("<H3>JURUSAN TEKNIK INFORMATIKA <H3>");
+            document.writeln("<HR WIDTH = 600 COLOR=GREEN>");
+            document.writeln("<H3>Alamat : Jl. Sigura - Gura No.2 Malang</H3>");
+            document.writeln("<H3>Telp : (0341) 551611</H3>");
+            document.writeln("</CENTER>")
+ 
